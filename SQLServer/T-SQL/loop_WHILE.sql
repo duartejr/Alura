33e8d40a0fea7eebefe34f92b9ae0060f6cd2e15
Duarte Junior <duarte.jr105@gmail.com>
2022-04-21 15:30:39 -0300

    Arquivos curso SQLServer 2017

---
+++ SQLServer/T-SQL/loop_WHILE.sql
@@ -0,0 +1,26 @@
+--Faça um script que, a partir do dia 01/01/2017, 
+--conte o número de notas fiscais até o dia 10/01/2017. 
+--Imprima a data e o número de notas fiscais.
+
+--Dicas:
+
+--Declare variáveis do tipo DATE: DATAINICIAL e DATAFINAL;
+--Faça um loop testando se a data inicial é menor que a data final;
+--Imprima a data e o número de notas na saída do Management Studio. 
+--Não esqueça de converter as variáveis para VARCHAR;
+--Acrescente um dia à data.
+
+DECLARE @DATAINICIAL DATE
+DECLARE @DATAFINAL DATE
+DECLARE @NUMNOTAS INT
+
+SET @DATAINICIAL = '20170101'
+SET @DATAFINAL = '20170110'
+
+WHILE @DATAINICIAL <= @DATAFINAL
+BEGIN
+	SELECT @NUMNOTAS = COUNT(*) FROM [NOTAS FISCAIS] WHERE DATA = @DATAINICIAL
+	PRINT CONVERT(VARCHAR(10), @DATAINICIAL) + ' --> ' + CONVERT(VARCHAR(10), @NUMNOTAS)
+	SELECT @DATAINICIAL = DATEADD(DAY, 1, @DATAINICIAL)
+
+END
---
+++ SQLServer/consultas/Funcoes+matemáticas.sql
@@ -0,0 +1,22 @@
+
+SELECT CEILING(12.333223)
+
+SELECT FLOOR(12.333223)
+
+SELECT RAND()
+
+SELECT ROUND(12.33323323, 2)
+
+SELECT * FROM [ITENS NOTAS FISCAIS]
+
+SELECT [QUANTIDADE], [PREÇO], ROUND(([QUANTIDADE] * [PREÇO]),1) FROM [ITENS NOTAS FISCAIS]
+
+--Na tabela de notas fiscais, temos o valor do imposto. Já na tabela de itens, temos a quantidade e o faturamento. 
+--Calcule o valor do imposto pago no ano de 2016, arredondando para o menor inteiro.
+
+SELECT YEAR(DATA), FLOOR(SUM(IMPOSTO * PREÇO * QUANTIDADE)) AS IMPOSTO
+FROM [NOTAS FISCAIS] NF
+INNER JOIN [ITENS NOTAS FISCAIS] INF
+ON NF.NUMERO = INF.NUMERO
+WHERE YEAR(DATA) = 2016
+GROUP BY YEAR(DATA)
