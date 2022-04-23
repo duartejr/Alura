a336be1506d6d2dc81664bb70ae953add7beff47
Duarte Junior <duarte.jr105@gmail.com>
2022-04-23 13:14:28 -0300

    comendos de teclado

--- InicianteProgramacao/javascript_para_web/aluramidi/main.js
+++ InicianteProgramacao/javascript_para_web/aluramidi/main.js
@@ -4,12 +4,24 @@ function tocaSom(idElementoAudio){
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
 
-while(contador < listaDeTeclas.length){
-    listaDeTeclas[contador].onclick = function () {
-        tocaSom('#som_tecla_pom');
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
     }
-    contador++;
-    console.log(contador);
+
+    tecla.onkeydown = function (event) {
+
+        if (event.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
